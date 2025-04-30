5f5364dab67f2a89c7cd9ca2d5460ca824b87d5b
Coffee_branch <150142524+blockchainMania@users.noreply.github.com>
2025-04-30 16:09:15 +0900

    comment 수정 3차

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ elif st.session_state.page == "curriculum":
     df_comment = pd.read_csv("커리큘럼_수정.csv", encoding="utf-8")
 
     comment_row = df_comment[(df_comment["학과"] == st.session_state.selected_major) & (df_comment["코멘트"].notna())]
-    comment_text = comment_row["코멘트"] if not comment_row.empty else None
+    comment_text = comment_row.iloc[0]["코멘트"] if not comment_row.empty else None
 
     st.title("📘 맞춤형 커리큘럼 및 입결 정보")
     st.markdown(f"선택한 학과: **{st.session_state.selected_major}**")
